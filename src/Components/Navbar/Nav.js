978a3adc3465b88ef98c90d0faa8bc66ec8ebfce
JIBON <jibon2230@gmail.com>
2022-10-11 15:41:41 +0600

    correct ans button added

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -9,7 +9,7 @@ export const Nav = () => {
             <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                 <div className="relative flex items-center justify-between">
                     <div className="flex items-center">
-                        <NavLink
+                        <NavLink  
                             to="/"
                             aria-label="company"
                             title="Jibon"
--- src/Components/SingleQuestions/SingleQuestions.js
+++ src/Components/SingleQuestions/SingleQuestions.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const SingleQuestions = ({ question, correctBtn }) => {
+    const correctAnsBtn = () => {
 
+        alert(question.correctAnswer)
+
+
+    }
 
     return (
         <div className='border shadow-2xl p-5 m-5 '>
-            <h1 className='bg-orange-400 mt-5 lg:h-20 rounded-lg text-xl' >{question.question}</h1>
+            <div className='flex justify-between relative'>
+                <h1 className='bg-orange-400 mt-5 lg:h-20 rounded-lg text-xl w-full' >{question.question}</h1>
+                <button onClick={correctAnsBtn} className='absolute top-[-3px] left-[1400px] hover:bg-red-600'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+                </button>
+            </div>
             <div className='lg:grid grid-cols-2 lg:h-32 lg:mt-2 '>
                 <h2 className='bg-purple-100 rounded-full shadow-xl mb-2 text-lg  ' onClick={() => correctBtn(question?.options[0])}>  <input type="radio" value="Male" name="gender" /> {question?.options[0]}</h2>
                 <h2 className='bg-purple-100 rounded-full shadow-xl mb-2 text-lg' onClick={() => correctBtn(question?.options[1])}>  <input type="radio" value="Male" name="gender" /> {question?.options[1]}</h2>
